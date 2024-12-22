4c9e00411c7a66c868a11f828d266229139add7e
Bayu Kevin Farindra <bayukevinfarindra23@gmail.com>
2024-12-23 02:40:55 +0700

    update

--- Assets/Js/slider.js
+++ Assets/Js/slider.js
@@ -1,21 +1,18 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 4, // Default: Show 4 slides at once
-    spaceBetween: 10, // Space between slides
+    slidesPerView: 5, 
+    spaceBetween: 10, 
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    loop: true, // Infinite loop
+    loop: true, 
     breakpoints: {
-        // Untuk ukuran layar lebih besar atau sama dengan 1024px (desktop)
         1024: {
             slidesPerView: 4, // 4 slides
         },
-        // Untuk ukuran layar lebih besar atau sama dengan 768px (tablet)
         768: {
             slidesPerView: 2, // 2 slides
         },
-        // Untuk ukuran layar lebih kecil dari 768px (mobile)
         0: {
             slidesPerView: 1, // 1 slide
         }
